ab81f6ebd2815c1950ecab479a89d24b2c3c7a04
khoa070104 <Khoablack71@gmail.com>
2024-10-29 07:39:28 +0700

    config_done

---
+++ FunnyCafeManagerment_DataAccess/ViewModels/CategoryVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunnyCafeManagerment_DataAccess.ViewModels
+{
+    internal class CategoryVM
+    {
+    }
+}
